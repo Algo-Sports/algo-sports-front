bc295814f3b9749caa08e43587ae7582cb6a82e6
shinkeonkim <singun11@naver.com>
2020-10-10 22:02:39 +0900

    feat: Router 적용

--- pages/layouts/siderLayout.js
+++ pages/layouts/siderLayout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from '../../styles/siderLayout.module.css'
 import { Layout, Menu, Breadcrumb } from 'antd';
 import Link from 'next/link';
+import Router from 'next/router'
 import {
   DotChartOutlined,
   RiseOutlined,
@@ -14,14 +15,15 @@ const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 class SiderLayout extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
+  
   state = {
     collapsed: false,
   };
+
+  constructor(props) {
+    super(props);
+    this.location = this.props.location;
+  }
 
   onCollapse = collapsed => {
     this.setState({ collapsed });
@@ -32,48 +34,24 @@ class SiderLayout extends Component {
     return (
       <Layout style={{ minHeight: '100vh' }}>
         <Sider className={styles.leftSideNav} collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-          <Link href = "/">
-            <a>
-              <div className={styles.logo} />
-            </a>
-          </Link>
+          <div className={styles.logo} onClick  = {() => Router.push('/')}/>
           <Menu className={styles.leftSideNavMenu} mode="inline">
-            <Menu.Item className={styles.leftSideNavMenuItem} key="1" icon={<UserOutlined />}>
-              <Link href="/profile">
-                <a>
-                  username
-                </a>
-              </Link>
+            <Menu.Item className={styles.leftSideNavMenuItem} key="1" icon={<UserOutlined />} onClick  = {() => Router.push('/profile')}>
+              username
             </Menu.Item>
             <SubMenu className={styles.leftSideNavSubMenu} key="GameOption" icon={<DotChartOutlined />} title="Game">
-              <Menu.Item className={styles.leftSideNavSubMenuItem} key="gameList">
-                <Link href = "/gamelist">
-                  <a>
-                    Game List
-                  </a>
-                </Link>
+              <Menu.Item className={styles.leftSideNavSubMenuItem} key="gameList" onClick  = {() => Router.push('/gamelist')}>
+                Game List
               </Menu.Item>
-              <Menu.Item className={styles.leftSideNavSubMenuItem} key="createGame">
-                <Link href = "/createGame">
-                  <a>
-                    Create Game
-                  </a>
-                </Link>
+              <Menu.Item className={styles.leftSideNavSubMenuItem} key="createGame" onClick  = {() => Router.push('/createGame')}>
+                Create Game
               </Menu.Item>
             </SubMenu>
-            <Menu.Item className={styles.leftSideNavMenuItem} key="2" icon={<RiseOutlined />}>
-              <Link href = "/ranking">
-                  <a>
-                    Ranking
-                  </a>
-                </Link>
+            <Menu.Item className={styles.leftSideNavMenuItem} key="2" icon={<RiseOutlined />} onClick  = {() => Router.push('/ranking')}>
+              Ranking
             </Menu.Item>
-            <Menu.Item className={styles.leftSideNavMenuItem} key="3" icon={<TrophyOutlined />}>
-              <Link href = "/award">
-                  <a>
-                    Award
-                  </a>
-                </Link>
+            <Menu.Item className={styles.leftSideNavMenuItem} key="3" icon={<TrophyOutlined />} onClick  = {() => Router.push('/award')}>
+              Award
             </Menu.Item>
 
           </Menu>
@@ -90,7 +68,7 @@ class SiderLayout extends Component {
               {this.props.children}
             </div>
           </Content>
-          <Footer className={styles.footer}> Algo sports ©2020 Created by KMU Algo Sports</Footer>
+    <Footer className={styles.footer}> Algo sports ©2020 Created by KMU Algo Sports {this.location}</Footer>
         </Layout>
       </Layout>
     )
