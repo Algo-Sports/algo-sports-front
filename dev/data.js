967c1151368a06724e23ce7ffeed6fefc16ed32a
shinkeonkim <singun11@naver.com>
2020-11-15 22:01:15 +0900

    feat: dev data

---
+++ dev/data.js
@@ -0,0 +1,63 @@
+export const onGoingGame = [
+    {
+        "name" : "AAAA Game",
+        "description" : "Where is A? Find them!",
+        "endDate" : "2020.12.31 17:00"
+    },
+    {
+        "name": "BBBB Game",
+        "description" : "Where is B? Find them!",
+        "endDate" : "2020.12.31 17:00"
+    },
+    {
+        "name": "BBBB Game",
+        "description" : "Where is B? Find them!",
+        "endDate" : "2020.12.31 17:00"
+    },
+    {
+        "name": "BBBB Game",
+        "description" : "Where is B? Find them!",
+        "endDate" : "2020.12.31 17:00"
+    },
+    {
+        "name": "BBBB Game",
+        "description" : "Where is B? Find them!",
+        "endDate" : "2020.12.31 17:00"
+    },
+    {
+        "name": "BBBB Game",
+        "description" : "Where is B? Find them!",
+        "endDate" : "2020.12.31 17:00"
+    }
+]
+
+
+export const siteInformation = [
+    {
+        "description": "Compiler info: g++ x.x.x",
+    },
+    {
+        "description" : "use standard in/output",
+    }
+]
+
+export const recentBlog = [
+    {
+        "description" : "Hello, ALGO SPORTS! COMPETE WITH PEOPLE",
+        "date" : "2020.11.20 17:20"
+    },
+    {
+        "description" : "Hello, ALGO SPORTS! COMPETE WITH PEOPLE222",
+        "date" : "2020.11.20 17:20"
+    },
+]
+
+export const updateLog = [
+    {
+        "description": "ver 0.0 : initial site setting",
+    },
+    {
+        "description" : "ver 0.1: bug fix",
+    }
+
+]
