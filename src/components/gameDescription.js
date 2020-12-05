1bbc293ba950d75a5e75b18e0d719a90e003e7eb
shinkeonkim <singun11@naver.com>
2020-12-06 01:12:07 +0900

    feat: Game description scroll bar

--- src/components/gameDescription.js
+++ src/components/gameDescription.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react'
+import Scrollbars from 'react-custom-scrollbars';
 import PdfViewer from './pdfViewer'
 
 class GameDescription extends Component {
 
-    render() {
-        const {game} = this.props;
-        return (
-            <div className = {"game-description-container"}>
-                <h2>
-                    {game.game_name}
-                </h2>
-                <hr/>
-                <PdfViewer filename = {game.game_content} />
-            </div>
-        )
-    }
+  render() {
+    const { game } = this.props;
+    return (
+
+      <Scrollbars 
+        style={{ height: "100%", minHeight: "300px"}}
+      >
+        <div className={"game-description-container"}>
+          <h2>
+            {game.game_name}
+          </h2>
+          <hr />
+          <PdfViewer filename={game.game_content} />
+        </div>
+      </Scrollbars>
+    )
+  }
 }
 
 
