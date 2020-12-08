93572ef3214547583ecdba64e5bdeadaf076b583
shinkeonkim <singun11@naver.com>
2020-12-08 19:08:20 +0900

    feat: seperate award data

--- src/pages/awardList.js
+++ src/pages/awardList.js
@@ -1,51 +1,39 @@
 import { TrophyOutlined } from '@ant-design/icons';
+import { render } from '@testing-library/react';
 import { Col, Row } from 'antd';
 import React, { Component } from 'react';
 import Banner from '../components/banner';
 import BaseLayout from '../layouts/baseLayout';
 
+import {awradList} from '../data/data';
+
+class Award extends Component {
+    render() {
+        const {award, title} = this.props;
+        return (
+            <Col lg = {{span: 6}} md = {{span: 8}} sm = {{span: 12}} xs = {{span: 12}} style = {{textAlign: "center", color: award?"#ffd700":"white"}}>
+                <TrophyOutlined style = {{fontSize: 100}}/><br/>
+                {title}
+            </Col>
+        )
+    }
+}
+
 class AwardList extends Component {
     render() {
+        const award = awradList.map(
+            item => {
+                return (
+                    <Award key = {item.id} title = {item.title} award = {item.award}/>
+                )
+            }
+        );
+
         return (
             <BaseLayout>
                 <Banner title="Award" subtitle="Who am I?" message="Check your Achievement" />
                 <Row gutter = {[20, 40]} className = "padding-30">
-                    <Col lg = {{span: 6}} md = {{span: 8}} sm = {{span: 12}} xs = {{span: 12}} style = {{textAlign: "center", color:"#ffd700"}}>
-                        <TrophyOutlined style = {{fontSize: 100}}/><br/>
-                        Beta Tester
-                    </Col>
-                    <Col lg = {{span: 6}} md = {{span: 8}} sm = {{span: 12}} xs = {{span: 12}} style = {{textAlign: "center", color:"white"}}>
-                        <TrophyOutlined style = {{fontSize: 100}}/><br/>
-                        게임 10개 이상 참가하기
-                    </Col>
-                    <Col lg = {{span: 6}} md = {{span: 8}} sm = {{span: 12}} xs = {{span: 12}} style = {{textAlign: "center", color:"white"}}>
-                        <TrophyOutlined style = {{fontSize: 100}}/><br/>
-                        게임 15개 이상 참가하기
-                    </Col>
-                    <Col lg = {{span: 6}} md = {{span: 8}} sm = {{span: 12}} xs = {{span: 12}} style = {{textAlign: "center", color:"white"}}>
-                        <TrophyOutlined style = {{fontSize: 100}}/><br/>
-                        게임 20개 이상 참가하기
-                    </Col>
-                    <Col lg = {{span: 6}} md = {{span: 8}} sm = {{span: 12}} xs = {{span: 12}} style = {{textAlign: "center", color:"white"}}>
-                        <TrophyOutlined style = {{fontSize: 100}}/><br/>
-                        게임 30개 이상 참가하기
-                    </Col>
-                    <Col lg = {{span: 6}} md = {{span: 8}} sm = {{span: 12}} xs = {{span: 12}} style = {{textAlign: "center", color:"white"}}>
-                        <TrophyOutlined style = {{fontSize: 100}}/><br/>
-                        게임 50개 이상 참가하기
-                    </Col>
-                    <Col lg = {{span: 6}} md = {{span: 8}} sm = {{span: 12}} xs = {{span: 12}} style = {{textAlign: "center", color:"white"}}>
-                        <TrophyOutlined style = {{fontSize: 100}}/><br/>
-                        게임 100개 이상 참가하기
-                    </Col>
-                    <Col lg = {{span: 6}} md = {{span: 8}} sm = {{span: 12}} xs = {{span: 12}} style = {{textAlign: "center", color:"white"}}>
-                        <TrophyOutlined style = {{fontSize: 100}}/><br/>
-                        TOP 10
-                    </Col>
-                    <Col lg = {{span: 6}} md = {{span: 8}} sm = {{span: 12}} xs = {{span: 12}} style = {{textAlign: "center", color:"white"}}>
-                        <TrophyOutlined style = {{fontSize: 100}}/><br/>
-                        TOP 100
-                    </Col>
+                    {award}
                 </Row>
             </BaseLayout>
         )
