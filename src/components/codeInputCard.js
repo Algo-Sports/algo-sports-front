ba7060bf569a1a8737ae7ae5a1688f1cd615c250
Aqudi <gjdigj145@gmail.com>
2020-12-14 03:30:07 +0900

    feat: Load language default template
    gameContentLayout에서 game 을 props로 전달
    codeInputCard 에서 game의 parameter값을 조회하여 기본 코드를 조회

--- src/components/codeInputCard.js
+++ src/components/codeInputCard.js
@@ -5,43 +5,104 @@ import { Row, Col, Tabs } from 'antd';
 import Select from 'react-select';
 const { TabPane } = Tabs;
 
+import { authHeader, handleTokenResponse } from '../_helpers';
+import { BASE_API_URL } from '../_constants';
+
 const options = [
-  { value: 'python', label: 'python' },
-  { value: 'c', label: 'c' },
-  { value: 'cpp', label: 'c++' }
+  {
+    value: 'python', label: 'Python (3.8.1)',
+    language_code: 38,
+  },
+  {
+    value: 'c', label: 'C (GCC 9.2.0)',
+    language_code: 7,
+  },
+  {
+    value: 'cpp', label: 'C++ (GCC 9.2.0)',
+    language_code: 9,
+  }
 ]
 
 class CodeInputCard extends Component {
   state = {
-    lang : "python",
+    lang: options[0],
+    parameters: {}
   }
 
   handleChange = lang => {
-    console.log(lang);
-    this.setState({lang: lang})
+    const newLang = options.find((item) => {
+      return item === lang;
+    })
+    this.setState({ ...this.state, lang: newLang });
+    this.getTemplateCode(newLang);
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    const { lang, parameters } = this.state;
+    console.log(parameters)
+    this.getTemplateCode(lang);
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("getDerivedStateFromProps");
+    if (nextProps.game !== prevState.game) {
+      console.log(nextProps.game.gameversion?.default_setting.parameters ?? {})
+      return {
+        ...prevState,
+        parameters: nextProps.game.gameversion?.default_setting.parameters ?? {}
+      }
+    }
+    return null;
   }
 
   editorDidMount(editor, monaco) {
     editor.focus();
   }
 
+  getTemplateCode(lang) {
+    console.log("getTemplateCode")
+    const { parameters } = this.state;
+    const { setCode } = this.props;
+
+    var requestOptions = {
+      method: 'POST',
+      body: JSON.stringify({ "parameters": parameters[lang.label] ?? [] }),
+      headers: authHeader(),
+    }
+
+    fetch(`${BASE_API_URL}/codes/programming-language/${lang.language_code}/get_solution_template/`, requestOptions)
+      .then(res => handleTokenResponse(res, `${BASE_API_URL}/codes/programming-language/${lang.language_code}/get_solution_template/`, requestOptions))
+      .then(res => {
+        console.log(res);
+        setCode(res.template_code);
+        return res;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
   render() {
-    const {lang} = this.state;
-    const {code, setCode} = this.props;
+    const { lang } = this.state;
+    const { code, setCode } = this.props;
+
+    // console.log(JSON.stringify(this.props))
+
     return (
       <Tabs type="card" defaultActiveKey="1" className="gameDetailTab height-100">
-        <TabPane tab="Code" key="1" className = "height-100" calssName = "padding-10">
-          
+        <TabPane tab="Code" key="1" className="height-100" calssName="padding-10">
+
           <Scrollbars
-            className = "height-100" 
-            style={{ minHeight: "300px"}}
+            className="height-100"
+            style={{ minHeight: "300px" }}
           >
-            <div className = "height-100 padding-10">
+            <div className="height-100 padding-10">
               <MonacoEditor
                 language={lang.value}
                 value={code}
-                height = "90%"        
-                editorDidMount={this.editorDidMount.bind(this)}        
+                height="90%"
+                editorDidMount={this.editorDidMount.bind(this)}
                 onChange={setCode}
                 options={{
                   autoIndent: "advanced",
@@ -52,31 +113,32 @@ class CodeInputCard extends Component {
                   snippetSuggestions: "bottom",
                   automaticLayout: false,
                   theme: 'vs-dark',
+                  defaultValue: "asdass",
                 }}
               />
               <Row>
-                <Col xs = {{span: 18}} className = "padding-20">
+                <Col xs={{ span: 18 }} className="padding-20">
                   <button
-                    className = "width-100"
+                    className="width-100"
                     style={{ backgroundColor: "#465580", color: "white", border: "none", height: "50px", fontSize: "1.3rem" }}
                     onClick={this.props.submitCode}
                   >
                     Submit Code!
                   </button>
                 </Col>
-                <Col xs = {{span: 6}}  className = "padding-20">
+                <Col xs={{ span: 6 }} className="padding-20">
                   <Select
                     name="lang"
                     defaultValue={options[0]}
                     options={options}
-                    value = {lang}
+                    value={lang}
                     isSearchable={true}
                     onChange={this.handleChange}
                   />
                 </Col>
               </Row>
             </div>
-            
+
           </Scrollbars>
         </TabPane>
       </Tabs>
--- src/layouts/gameContentLayout.js
+++ src/layouts/gameContentLayout.js
@@ -8,8 +8,8 @@ import CodeInputCard from '../components/codeInputCard';
 
 class GameContentLayout extends Component {
   state = {
-    code : "",
-    lang : "",
+    code: "",
+    lang: "",
     langList: [
       {
         id: 1,
@@ -21,7 +21,7 @@ class GameContentLayout extends Component {
       },
       {
         id: 3,
-        name: "c", 
+        name: "c",
       },
     ],
   }
@@ -31,7 +31,7 @@ class GameContentLayout extends Component {
     this.handleCode = this.handleCode.bind(this);
     this.submitCode = this.submitCode.bind(this);
   }
-  
+
   componentDidMount() {
   }
 
@@ -43,39 +43,39 @@ class GameContentLayout extends Component {
   }
 
   submitCode() {
-    const {code} = this.state;
-    const {game} = this.props;
+    const { code } = this.state;
+    const { game } = this.props;
 
     console.log(JSON.stringify(game));
-    console.log(JSON.stringify({'code': code}));
+    console.log(JSON.stringify({ 'code': code }));
   }
 
   render() {
-    const {game, ranking, gameResult, loading} = this.props;
+    const { game, ranking, gameResult, loading } = this.props;
     return (
       <>
-        <nav className = "game-detail-nav">
+        <nav className="game-detail-nav">
           <Link to="/gamelist">
-            <h1 className = "line-height-60">
+            <h1 className="line-height-60">
               &lt; Back to Game List
             </h1>
           </Link>
         </nav>
-          <Row className = "padding-20 min-height-90">
-            <Col lg={{ span: 0 }} md={{ span: 0 }} sm={{ span: 1 }} xs={{ span: 1 }} />
-              <Col
-                lg={{ span: 12 }} md={{ span: 12 }} sm={{ span: 22 }} xs={{ span: 22 }}
-                className = "outer-card-layout">
-                <GameDetail game = {game} ranking = {ranking} gameResult = {gameResult} loading = {loading.game_content}/>
-              </Col>
-              <Col lg={{ span: 0 }} md={{ span: 0 }} sm={{ span: 1 }} xs={{ span: 1 }} />
-              <Col
-                lg={{ span: 12}} md={{ span: 12 }} sm={{ span: 22 }} xs={{ span: 22 }}
-                className = "outer-card-layout">
-                <CodeInputCard code = {this.state.code} setCode = {this.handleCode} submitCode = {this.submitCode}/>
-              </Col>
-            <Col lg={{ span: 0 }} md={{ span: 0 }} sm={{ span: 1 }} xs={{ span: 1 }} />
-          </Row>
+        <Row className="padding-20 min-height-90">
+          <Col lg={{ span: 0 }} md={{ span: 0 }} sm={{ span: 1 }} xs={{ span: 1 }} />
+          <Col
+            lg={{ span: 12 }} md={{ span: 12 }} sm={{ span: 22 }} xs={{ span: 22 }}
+            className="outer-card-layout">
+            <GameDetail game={game} ranking={ranking} gameResult={gameResult} loading={loading.game_content} />
+          </Col>
+          <Col lg={{ span: 0 }} md={{ span: 0 }} sm={{ span: 1 }} xs={{ span: 1 }} />
+          <Col
+            lg={{ span: 12 }} md={{ span: 12 }} sm={{ span: 22 }} xs={{ span: 22 }}
+            className="outer-card-layout">
+            <CodeInputCard game={game} code={this.state.code} setCode={this.handleCode} submitCode={this.submitCode} />
+          </Col>
+          <Col lg={{ span: 0 }} md={{ span: 0 }} sm={{ span: 1 }} xs={{ span: 1 }} />
+        </Row>
       </>
     )
   }
