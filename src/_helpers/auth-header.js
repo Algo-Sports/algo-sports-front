fba0fcc0e439f2a1d02d0072644a2d8b7ada1c59
shinkeonkim <singun11@naver.com>
2020-12-14 00:23:05 +0900

    fix: content type issue

--- src/_helpers/auth-header.js
+++ src/_helpers/auth-header.js
@@ -6,9 +6,9 @@ export function authHeader() {
     let user = JSON.parse(localStorage.getItem('user'));
 
     if (user && user.access_token) {
-        return { 'Content-Type': 'text/plain', 'Authorization': 'Bearer ' + user.access_token};
+        return { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + user.access_token};
     } else {
-        return { 'Content-Type': 'text/plain', 'Authorization': 'Bearer '};
+        return { 'Content-Type': 'application/json', 'Authorization': 'Bearer '};
     }
 }
 
